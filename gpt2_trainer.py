7848ef5646abb4df839ea840d562a873f1942bd6
akbarsigit <akbarsigit3@gmail.com>
2024-07-17 20:09:32 +0700

    fix for raw model module for optimizer

--- gpt2_trainer.py
+++ gpt2_trainer.py
@@ -406,6 +406,7 @@ model.to(device)
 model = torch.compile(model) # compile for the NN => like GCC for c/c++ code => for speedup
 if ddp:
     model = DDP(model, device_ids=[ddp_local_rank])
+raw_model = model.module if ddp else model
 
 # doing learning rate scheduling
 max_lr = 6e-4
@@ -430,7 +431,7 @@ def get_lr(it):
 
 # this params is got from GPT3 paper
 # optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
-optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device)
+optimizer = raw_model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device)
 
 for step in range(max_steps):
     t0 = time.time()
